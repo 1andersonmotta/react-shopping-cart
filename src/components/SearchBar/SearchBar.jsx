7d4127179990740a126da19067585fba75734865
1andersonmotta <devandersonmotta@gmail.com>
2023-09-08 13:53:23 -0300

    fix adjusts

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,11 +1,21 @@
+import { React, useState } from "react";
 import { BsSearch } from "react-icons/bs"
-import React from "react";
 import './SearchBar.css'
 
 function SearchBar() {
+
+    const [searchValue, setSearchValue] = useState('');
+
     return (
         <form className="search-bar" >
-            <input type="search" placeholder="Buscar Produto" className="search__input" required />
+            <input
+                type="search"
+                value={searchValue}
+                placeholder="Buscar Produto"
+                className="search__input"
+                onChange={({ target }) => setSearchValue(target.value)}
+                required
+            />
             <button type="submit" className="search__button">
                 <BsSearch />
             </button>
