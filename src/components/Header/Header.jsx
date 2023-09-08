2df11362b43afcc9bd17ef835b45c5717aa0ec5b
1andersonmotta <devandersonmotta@gmail.com>
2023-09-08 13:51:39 -0300

    feature header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import SearchBar from "../SearchBar/SearchBar";
 import './Header.css'
+import CartButton from "../CartButton/CartButton";
+
+
 
 function Header() {
     return (
         <header className="header">
             <div className="container">
 
-                <SearchBar />
-
+                <SearchBar></SearchBar>
+                <CartButton />
             </div>
         </header>
     )
