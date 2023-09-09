45870e2ce9cff986341164608de70515214f056c
1andersonmotta <devandersonmotta@gmail.com>
2023-09-09 12:08:55 -0300

    fix cartButton ident

--- src/components/CartButton/CartButton.jsx
+++ src/components/CartButton/CartButton.jsx
@@ -10,7 +10,10 @@ function CartButton() {
     const { cartItems, isCartVisible, setIsCartVisible } = useContext(AppContext)
 
     return (
-        <button type="button" className="cart__button" onClick={() => setIsCartVisible(!isCartVisible)}>
+        <button
+            type="button"
+            className="cart__button"
+            onClick={() => setIsCartVisible(!isCartVisible)}>
             <AiOutlineShoppingCart />
             {cartItems.length > 0 && <span className="cart-status">{cartItems.length}</span>}
         </button>
